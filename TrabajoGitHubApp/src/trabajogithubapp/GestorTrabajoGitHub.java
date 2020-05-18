5f146ef6cece3a0dfe1700fc79c7eb535d6aa1db
Tatimga <tmgualotunia@espe.edu.ec>
2020-05-18 06:17:17 -0500

    Primer Commit TrabajoGitHub

---
+++ TrabajoGitHubApp/src/trabajogithubapp/GestorTrabajoGitHub.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajogithubapp;
+
+/**
+ *
+ * @author tatianagualotuna
+ */
+public class GestorTrabajoGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
